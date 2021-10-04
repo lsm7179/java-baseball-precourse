8a006a21696535dd11e4982db942267b703b9cd5
이승민 <sml7179@naver.com>
2021-10-04 13:40:16 +0900

    refactor: 랜덤 숫자 생성 로직 리팩토링
    기존에 get으로 가져와서 중복번호 방지하는 로직을 list.contains를 사용하여 중복번호 방지하도록 변경

--- src/main/java/baseball/model/RandomNumber.java
+++ src/main/java/baseball/model/RandomNumber.java
@@ -14,33 +14,15 @@ public class RandomNumber {
 
     public List<Integer> make() {
         for (int i = 0; i < 3; i++) {
-            randomNumbers.add(numbers(i));
+            randomNumbers.add(addNumber());
         }
         return randomNumbers;
     }
 
-    private Integer numbers(int index) {
-        if (index == 0) {
-            return Randoms.pickNumberInRange(1, 9);
-        }
-        if (index == 1) {
-            return addSecondNumber();
-        }
-        return addThirdNumber();
-    }
-
-    private Integer addSecondNumber() {
+    private Integer addNumber() {
         int randomNumber = Randoms.pickNumberInRange(1, 9);
-        if (randomNumbers.get(0) == randomNumber) {
-            return addSecondNumber();
-        }
-        return randomNumber;
-    }
-
-    private Integer addThirdNumber() {
-        int randomNumber = addSecondNumber();
-        if(randomNumbers.get(1) == randomNumber){
-            return addThirdNumber();
+        if (randomNumbers.contains(randomNumber)) {
+            return addNumber();
         }
         return randomNumber;
     }
